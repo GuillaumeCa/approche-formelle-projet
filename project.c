a5d1308b5e76308cb60d66c63696ef8e19a9f7fb
GuillaumeCa <guillaume.carre@isep.fr>
2017-11-19 19:47:04 +0100

    add proofs

--- project.c
+++ project.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
 /*@
-  predicate sorted{L}(int *arr, int length) =
-    \forall integer i, j; 0 <= i <= j < length ==> arr[i] <= arr[j];
+  predicate 
+    Sorted{L}(int *arr, integer start, integer end) =
+      \forall integer i, j; start <= i <= j < end ==> arr[i] <= arr[j];
+  
+  predicate 
+    Sorted{L}(int *arr, integer length) =
+      Sorted{L}(arr, 0, length - 1);
 */
 
 /*@ 
@@ -25,21 +30,27 @@ void swap(int *a, int *b) {
    
 /*@ 
   requires \valid(t+(low..high));
-  ensures \forall integer n; n <= \result ==> t[n] <= high;
+  assigns t[low..high];
 */
+// ensures \forall integer n; n <= \result ==> t[n] <= high;
 int partition(int *t, int low, int high) {
   int pivot = t[high];    // pivot
   int i = low;  // Index of smaller element
   
   /*@
-    loop invariant low <= j < high && low <= i < high - low;
-    loop assigns j, i;
-    loop variant j - i;
+    loop invariant low <= j <= high;
+    loop invariant low <= i <= high;
+    loop invariant i <= j;
+    loop assigns j, i, t[low..high];
+    loop variant high-j;
   */
   for (int j = low; j < high; j++) {
     // If current element is smaller than or
     // equal to pivot
     if (t[j] <= pivot) {
+      /*@
+        assigns t[i], t[j];
+      */
       swap(&t[i], &t[j]);
       i++;    // increment index of smaller element
     }
@@ -48,41 +59,13 @@ int partition(int *t, int low, int high) {
   return i;
 }
  
-/* The main function that implements QuickSort
- arr[] --> Array to be sorted,
-  low  --> Starting index,
-  high  --> Ending index */
-
-/*@ 
-  requires \valid(t+(low..high));
-  ensures \forall integer i, j; low <= i <= j < high ==> t[i] <= t[j];
+/* 
+  The main function that implements QuickSort
+  arr[] --> Array to be sorted,
+  low   --> Starting index,
+  high  --> Ending index 
 */
-void quickSort(int *t, int low, int high) {
-  if (low < high) {
-    /* pi is partitioning index, arr[p] is now
-      at right place */
-    int pi = partition(t, low, high);
 
-    // Separately sort elements before
-    // partition and after partition
-    
-    quickSort(t, low, pi - 1);
-    
-    quickSort(t, pi + 1, high);
-  }
-}
-
-/*@ 
-  requires \valid(t+(0..l-1));
-  ensures sorted(t, l);
-*/
-void sort(int *t, int l) {
-  /*@ 
-    requires \valid(t+(0..l-1));
-    ensures \forall integer i, j; 0 <= i <= j < l-1 ==> t[i] <= t[j];
-  */
-  quickSort(t, 0, l - 1);
-}
 
 void affiche(int *t, int l) { 
   int i;
@@ -90,7 +73,34 @@ void affiche(int *t, int l) {
   for (i=0; i<l-1; i++) {
     printf("%d, ",t[i]); 
   }
-  printf("]\n"); 
+  printf("%d ]\n", t[i]); 
+}
+
+/*@ 
+  requires  \valid(t + (low..high));
+  ensures   Sorted(t, low, high);
+*/
+void quickSort(int *t, int low, int high) {
+  if (low < high) {
+    /* pi is partitioning index, arr[p] is now
+      at right place */
+    int pi = partition(t, low, high);
+    // printf("from %d to %d, pi: %d\n", low, high, pi);
+    // affiche(t, 10);
+    // Separately sort elements before
+    // partition and after partition
+    
+    quickSort(t, low, pi - 1);
+    quickSort(t, pi + 1, high);
+  }
+}
+
+/*@ 
+  requires \valid(t+(0..l-1));
+  ensures Sorted(t, l);
+*/
+void sort(int *t, int l) {
+  quickSort(t, 0, l - 1);
 }
 
 
